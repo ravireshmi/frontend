78b66d48360dc03609326aff55c14e72bc9fc608
ravireshmi <ravireshmi51@gmail.com>
2024-03-09 03:58:52 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://foyr.com/learn/how-to-create-interior-design-website/" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://backend-henna-beta.vercel.app"
